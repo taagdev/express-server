d7cbe9ca217b7f87a71f172edec9fa40a4a0151f
Olatunde Garuba <generalgmt@yahoo.co.uk>
2016-06-08 15:48:33 +0100

    initial commit

--- app/routes/routes.server.js
+++ app/routes/routes.server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = function(app){
-var chat = require('../controllers/index.controller.server');
+var index = require('../controllers/index.controller.server');
 
-	app.route('/').get(chat.renderIndex);
+	app.route('/').get(index.renderIndex);
 
 };
